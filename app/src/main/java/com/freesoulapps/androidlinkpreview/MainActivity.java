1bd52358cde61fb2ff152c46cdcae9c07b939c5a
Alex <alex.v.2007@gmail.com>
2015-12-14 18:03:18 +0200

    Cleaning

--- app/src/main/java/com/freesoulapps/androidlinkpreview/MainActivity.java
+++ app/src/main/java/com/freesoulapps/androidlinkpreview/MainActivity.java
@@ -32,6 +32,5 @@ public class MainActivity extends Activity {
                     }
                 }
             });
-        //mPreview.setData("http://www.wallashops.co.il/%D7%98%D7%9C%D7%95%D7%95%D7%99%D7%96%D7%99%D7%94-50-LED-Smart-TV-NEON-%D7%93%D7%92%D7%9D-NE-50FLED/pi2ICNE50FLEDSMART");
     }
 }
